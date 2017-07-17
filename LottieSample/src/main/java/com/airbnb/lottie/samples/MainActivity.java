293859a2fee29d5ee4063510bb04d9aafe48ca3f
evan <liusuzheng2012@gmail.com>
2017-07-17 20:07:54 +0800

    blinding animation

--- LottieSample/src/main/java/com/airbnb/lottie/samples/MainActivity.java
+++ LottieSample/src/main/java/com/airbnb/lottie/samples/MainActivity.java
@@ -1,6 +1,9 @@
 package com.airbnb.lottie.samples;
 
-import android.animation.ValueAnimator;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -10,9 +13,9 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.SeekBar;
 
@@ -42,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
   float currentProgress = 0f;
 
+  private Button fineTuneButton;
+
 
 //    static String FILE_NAME = "Hello World.json";
 
@@ -69,6 +74,8 @@ public class MainActivity extends AppCompatActivity {
 
     bodyView = (FrameLayout) findViewById(R.id.body_container);
     headView = (FrameLayout) findViewById(R.id.head);
+
+    fineTuneButton = (Button) findViewById(R.id.fine_tune);
 
     eyebrow = findViewById(R.id.eyebrow);
 
@@ -117,52 +124,38 @@ public class MainActivity extends AppCompatActivity {
       public void onStopTrackingTouch(SeekBar seekBar) {
       }
     });
+
+    fineTuneButton.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+
+      }
+    });
   }
 
   private void updateEyeBrowPosition() {
+    FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) eyebrow.getLayoutParams();
 
-    // FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) eyebrow.getLayoutParams();
-    //
-    // Log.e(TAG, "updateEyeBrowPosition progress: " + currentProgress);
-    //
-    // float[] bottomPositions = {
-    //     220f, 220f, 220f, 220f, 220f,
-    //     220f, 220f, 220f, 226f, 230f,
-    //     240f, 245f, 260f, 270f, 285f,
-    //     295f, 310f, 315f, 325f, 340f};
-    //
-    // int position = (int) (currentProgress / 5 - 1);
-    //
-    // Log.e(TAG, "updateEyeBrowPosition: position" + position);
-    // float bottom = bottomPositions[position > 0 ? position : 0] * torsoView.getScale();
-    //
-    // Log.e(TAG, "updateEyeBrowPosition: bottom" + bottom );
-    // params.setMargins(params.leftMargin, params.topMargin, params.rightMargin,
-    //     (int) dipToPixels(bottom));
-    //
-    // eyebrow.setLayoutParams(params);
-  }
+    float[] bottomPositions = {
+        180f, 180f, 180f, 180f, 180f,
+        180f, 180f, 180f, 186f, 190f,
+        200f, 210f, 218f, 225f, 235f,
+        245f, 258f, 265f, 275f, 285f};
 
-  private void addBlinkAnimation() {
-    final ValueAnimator animator = ValueAnimator.ofInt(0, 100);
-    animator.setDuration(4000);
-    animator.setInterpolator(new LinearInterpolator());
-    animator.setRepeatCount(ValueAnimator.INFINITE);
-    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-      @Override public void onAnimationUpdate(ValueAnimator valueAnimator) {
-        Integer progress = (Integer) valueAnimator.getAnimatedValue();
-        if (progress > 95) {
-          eyebrow.setVisibility(View.VISIBLE);
-        } else {
-          eyebrow.setVisibility(View.GONE);
-        }
-      }
-    });
-    animator.start();
+    int position = (int) (currentProgress / 5 - 1);
+    float bottom = bottomPositions[position > 0 ? position : 0] * torsoView.getScale();
+    params.setMargins(params.leftMargin, params.topMargin, params.rightMargin,
+        (int) dipToPixels(bottom));
+
+    eyebrow.setLayoutParams(params);
   }
 
   private static float WOBBLE_DURATION = 3.0f;
 
+  /**
+   * change duration when change progress
+   *
+   * @return
+   */
   private float popAnimationsDuration() {
     return (float) (WOBBLE_DURATION - currentProgress / 100f * WOBBLE_DURATION * 0.6);
   }
@@ -179,6 +172,9 @@ public class MainActivity extends AppCompatActivity {
   }
 
 
+  /**
+   * add idle animations
+   */
   private void addIdleAnimations() {
     float wobbleDuration = this.popAnimationsDuration();
     Log.e(TAG, "wobbleDuration: " + wobbleDuration);
@@ -186,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
     // body wobbling
     float wobbleAngle = 3.0f;
 
-    RotateAnimation torsoAnim =
+    final RotateAnimation torsoAnim =
         new RotateAnimation(-wobbleAngle, wobbleAngle, Animation.RELATIVE_TO_SELF,
             0.5f,
             Animation.RELATIVE_TO_SELF, 0.7f);
@@ -198,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
     // // head rotation
     float headRotationAngle = 2.0f;
 
-    RotateAnimation headAnim =
+    final RotateAnimation headAnim =
         new RotateAnimation(-headRotationAngle, headRotationAngle,
             Animation.RELATIVE_TO_SELF,
             0.5f,
@@ -211,9 +207,9 @@ public class MainActivity extends AppCompatActivity {
     // arms
     float armsTranslationAmount = 18;
 
-    TranslateAnimation armAnim = new TranslateAnimation(
+    final TranslateAnimation armAnim = new TranslateAnimation(
         0f, 0f,
-         0f,
+        0f,
         armsTranslationAmount
     );
     armAnim.setRepeatCount(Animation.INFINITE);
@@ -224,10 +220,29 @@ public class MainActivity extends AppCompatActivity {
     // tongue translation
     // TODO
 
-
     // blink animation
-    // addBlinkAnimation();
 
+    addBlinkAnimation();
+  }
+
+  private void addBlinkAnimation() {
+    eyebrow.setPivotY(0);
+
+    ObjectAnimator downAnimation = ObjectAnimator.ofFloat(eyebrow, "scaleY", 0f, 1f);
+    downAnimation.setDuration(500);
+    ObjectAnimator upAnimation = ObjectAnimator.ofFloat(eyebrow, "scaleY", 1f, 0f);
+    upAnimation.setDuration(500);
+
+    AnimatorSet animatorSet = new AnimatorSet();
+    animatorSet.setStartDelay(3000);
+    animatorSet.playSequentially(downAnimation, upAnimation);
+    animatorSet.addListener(new AnimatorListenerAdapter() {
+      @Override public void onAnimationEnd(Animator animation) {
+        super.onAnimationEnd(animation);
+        addBlinkAnimation();
+      }
+    });
+    animatorSet.start();
   }
 
   @Override
